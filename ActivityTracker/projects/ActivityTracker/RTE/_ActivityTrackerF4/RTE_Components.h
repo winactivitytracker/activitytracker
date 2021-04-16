a8fbccd3e6e1993ebea83e7c3e7f8d796f23bf99
Mikisugi <toon.helmer@gmail.com>
2021-04-16 22:54:28 +0900

    clean up

--- ActivityTracker/projects/ActivityTracker/RTE/_ActivityTrackerF4/RTE_Components.h
+++ ActivityTracker/projects/ActivityTracker/RTE/_ActivityTrackerF4/RTE_Components.h
@@ -3,7 +3,7 @@
  * Auto generated Run-Time-Environment Configuration File
  *      *** Do not modify ! ***
  *
- * Project: 'workshop3' 
+ * Project: 'ActivityTracker' 
  * Target:  'ActivityTrackerF4' 
  */
 
--- ActivityTracker/projects/ActivityTracker/obj/startup_stm32f051x8.d
+++
@@ -1,1 +0,0 @@
-.\obj\startup_stm32f051x8.o: RTE\Device\STM32F051R8\startup_stm32f051x8.s
