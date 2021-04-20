7d1cc9d02706e999615c8dd03d67094d507d33d6
Mikisugi <toon.helmer@gmail.com>
2021-04-20 23:46:05 +0900

    stm32f4

--- ActivityTracker/projects/ActivityTracker/RTE/_ActivityTrackerF4/RTE_Components.h
+++ ActivityTracker/projects/ActivityTracker/RTE/_ActivityTrackerF4/RTE_Components.h
@@ -1,6 +1,6 @@
 
 /*
- * Auto generated Run-Time-Environment Configuration File
+ * Auto generated Run-Time-Environment Component Configuration File
  *      *** Do not modify ! ***
  *
  * Project: 'ActivityTracker' 
@@ -14,16 +14,11 @@
 /*
  * Define the Device Header File: 
  */
-#define CMSIS_device_header "stm32f0xx.h"
+#define CMSIS_device_header "stm32f4xx.h"
 
-/*  ARM.FreeRTOS::RTOS:Config:FreeRTOS:10.2.1 */
+#define RTE_DEVICE_STARTUP_STM32F4XX    /* Device Startup for STM32F4 */
 #define RTE_RTOS_FreeRTOS_CONFIG        /* RTOS FreeRTOS Config for FreeRTOS API */
-/*  ARM.FreeRTOS::RTOS:Core:Cortex-M:10.2.1 */
 #define RTE_RTOS_FreeRTOS_CORE          /* RTOS FreeRTOS Core */
-/*  ARM.FreeRTOS::RTOS:Heap:Heap_1:10.2.1 */
-#define RTE_RTOS_FreeRTOS_HEAP_1        /* RTOS FreeRTOS Heap 1 */
-/*  Keil.Standalone::Device:Startup:1.0.0 */
-#define RTE_DEVICE_STARTUP_STM32F0XX    /* Device Startup for STM32F0 */
-
+#define RTE_RTOS_FreeRTOS_HEAP_4        /* RTOS FreeRTOS Heap 4 */
 
 #endif /* RTE_COMPONENTS_H */
--- ActivityTracker/projects/ActivityTracker/src/mainF4.c
+++ ActivityTracker/projects/ActivityTracker/src/mainF4.c
@@ -1,5 +1,6 @@
-#include "stm32f0_discovery.h"
-#include "stm32f0xx.h"
+//#include "stm32f0_discovery.h"
+//#include "stm32f0xx.h"
+#include "stm32f4xx.h"
 #include <FreeRTOS.h>
 #include <task.h>
 
@@ -9,33 +10,73 @@ void initGpio(void);
 void vPeriodicTask(void *pvParameters)
 {
   // Establish the task's period.
-  const TickType_t xDelay = pdMS_TO_TICKS(1000);
+  const TickType_t xDelay = pdMS_TO_TICKS(200);
   TickType_t xLastWakeTime = xTaskGetTickCount();
   for (;;) {
     // Block until the next release time.
     vTaskDelayUntil(&xLastWakeTime, xDelay);
-		GPIO_WriteBit(GPIOB, GPIO_Pin_8, Bit_RESET);
-    GPIO_WriteBit(GPIOB, GPIO_Pin_9, Bit_SET);
+		//GPIO_WriteBit(GPIOB, GPIO_Pin_8, Bit_RESET);
+    //GPIO_WriteBit(GPIOB, GPIO_Pin_9, Bit_SET);
+		GPIOB->BSRR = GPIO_BSRR_BR_8;
+		GPIOB->BSRR = GPIO_BSRR_BS_9;
   }
 }
 
 void vPeriodicTask2(void *pvParameters)
 {
   // Establish the task's period.
-  const TickType_t xDelay = pdMS_TO_TICKS(700);
+  const TickType_t xDelay = pdMS_TO_TICKS(100);
   TickType_t xLastWakeTime = xTaskGetTickCount();
   for (;;) {
     // Block until the next release time.
     vTaskDelayUntil(&xLastWakeTime, xDelay);
-		GPIO_WriteBit(GPIOB, GPIO_Pin_8, Bit_RESET);
-    GPIO_WriteBit(GPIOB, GPIO_Pin_9, Bit_SET);
+		//GPIO_WriteBit(GPIOB, GPIO_Pin_8, Bit_RESET);
+    //GPIO_WriteBit(GPIOB, GPIO_Pin_9, Bit_SET);
+		GPIOB->BSRR = GPIO_BSRR_BR_9;
+		GPIOB->BSRR = GPIO_BSRR_BS_8;
   }
 }
 
 
 int main()
 {
-	initGpio();
+	
+  // GPIOC Periph clock enable
+  RCC->AHB1ENR |= RCC_AHB1ENR_GPIOBEN; 
+
+  // PC8 and PC9 in output mode
+  GPIOB->MODER |= (GPIO_MODER_MODER8_0 | GPIO_MODER_MODER9_0);
+
+  // Push pull mode selected
+  GPIOB->OTYPER &= ~(GPIO_OTYPER_OT_8 | GPIO_OTYPER_OT_9);
+
+  // Maximum speed setting
+  GPIOB->OSPEEDR |= (GPIO_OSPEEDER_OSPEEDR8 | GPIO_OSPEEDER_OSPEEDR9);
+
+  // Pull-up and pull-down resistors disabled
+  GPIOB->PUPDR &= ~(GPIO_PUPDR_PUPDR8 | GPIO_PUPDR_PUPDR9);
+
+  //GPIOB->BSRR = GPIO_BSRR_BS_9;
+  while(1)
+  {
+    // Set PC9
+		GPIOB->BSRR = GPIO_BSRR_BS_9;
+    // Reset PC8
+    GPIOB->BSRR = GPIO_BSRR_BR_8;
+    // Delay ~ 1 sec.
+    delay(SystemCoreClock/8);    
+    
+    // Reset PC9
+    GPIOB->BSRR = GPIO_BSRR_BR_9;
+    // Set PC8
+    GPIOB->BSRR = GPIO_BSRR_BS_8;
+    // Delay ~ 1 sec.
+    delay(SystemCoreClock/8);
+		
+  }
+	
+	
+//	initGpio();
 	
   xTaskCreate(vPeriodicTask, "My Task", 256, NULL, 1, NULL);
   xTaskCreate(vPeriodicTask2, "My Task2", 256, NULL, 2, NULL);
@@ -51,7 +92,7 @@ int main()
 }
 
 void initGpio(void){
-	GPIO_InitTypeDef gpioB;
+	/*GPIO_InitTypeDef gpioB;
 	
 	GPIO_StructInit(&gpioB);
 
@@ -62,5 +103,17 @@ void initGpio(void){
 	gpioB.GPIO_Speed    	= GPIO_Speed_Level_2;
 	gpioB.GPIO_Mode       = GPIO_Mode_OUT;
 
-	GPIO_Init(GPIOB, &gpioB);
+	GPIO_Init(GPIOB, &gpioB);*/
 }
+
+#pragma push
+#pragma O3
+void delay(const int d)
+{
+  volatile int i;
+
+  for(i=d; i>0; i--){ ; }
+
+  return;
+}
+#pragma pop
