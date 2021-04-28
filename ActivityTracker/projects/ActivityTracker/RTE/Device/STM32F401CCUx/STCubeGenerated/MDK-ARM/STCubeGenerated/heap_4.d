7fba41dfa13ff7fbfd586f4753cdde176fea0334
Mark van Erven <Markvanerven@gmail.com>
2021-04-28 14:30:14 +0200

    F4 WORKIE BOI

---
+++ ActivityTracker/projects/ActivityTracker/RTE/Device/STM32F401CCUx/STCubeGenerated/MDK-ARM/STCubeGenerated/heap_4.d
@@ -0,0 +1,13 @@
+stcubegenerated\heap_4.o: ../Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang/heap_4.c
+stcubegenerated\heap_4.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
+stcubegenerated\heap_4.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h
+stcubegenerated\heap_4.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
+stcubegenerated\heap_4.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+stcubegenerated\heap_4.o: ../Inc/FreeRTOSConfig.h
+stcubegenerated\heap_4.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h
+stcubegenerated\heap_4.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/portable.h
+stcubegenerated\heap_4.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h
+stcubegenerated\heap_4.o: ../Middlewares/Third_Party/FreeRTOS/Source/portable/RVDS/ARM_CM4F/portmacro.h
+stcubegenerated\heap_4.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h
+stcubegenerated\heap_4.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/task.h
+stcubegenerated\heap_4.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/list.h
---
+++ ActivityTracker/projects/ActivityTracker/RTE/Device/STM32F401CCUx/STCubeGenerated/MDK-ARM/STCubeGenerated/port.d
@@ -0,0 +1,12 @@
+stcubegenerated\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/portable/RVDS/ARM_CM4F/port.c
+stcubegenerated\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h
+stcubegenerated\port.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
+stcubegenerated\port.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+stcubegenerated\port.o: ../Inc/FreeRTOSConfig.h
+stcubegenerated\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h
+stcubegenerated\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/portable.h
+stcubegenerated\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h
+stcubegenerated\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/portable/RVDS/ARM_CM4F/portmacro.h
+stcubegenerated\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h
+stcubegenerated\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/task.h
+stcubegenerated\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/list.h
