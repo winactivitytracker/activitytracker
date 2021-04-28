4752453b4f26f7602117fe4f705f750e2a709577
Mark van Erven <Markvanerven@gmail.com>
2021-04-28 13:59:17 +0200

    EVERYTHING WORKS!!!

---
+++ ActivityTracker/projects/ActivityTracker/RTE/_Target_1/RTE_Components.h
@@ -0,0 +1,22 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'I2CHAL' 
+ * Target:  'Target 1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f0xx.h"
+
+#define RTE_DEVICE_CUBE_MX_HAL
+#define RTE_DEVICE_FRAMEWORK_CUBE_MX
+
+#endif /* RTE_COMPONENTS_H */
