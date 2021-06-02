9351cbfeec781bbaf0850f508445739526bc3357
Maarten de Jong <accounts@arteeh.com>
2021-06-02 09:37:05 +0200

    Remove build

--- F0/Debug/Shared/aes.d
+++
@@ -1,4 +0,0 @@
-Shared/aes.o: C:/School/IOT/KBS/GitHub/IoT/activitytracker/Shared/aes.c \
- C:/School/IOT/KBS/GitHub/IoT/activitytracker/Shared/aes.h
-
-C:/School/IOT/KBS/GitHub/IoT/activitytracker/Shared/aes.h:
